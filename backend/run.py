4789ba45638ec872235f0f8aa7bd8c31e633506c
ybenzou <ybenzou95@gmail.com>
2025-04-10 05:24:01 +0800

    deploy backend 0.1 add domain

--- backend/run.py
+++ backend/run.py
@@ -3,7 +3,11 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, ssl_context=(
-        '/home/ec2-user/myBlog/ssl/mysite.crt',
-        '/home/ec2-user/myBlog/ssl/mysite.key'
-    ))
+    app.run(
+        host='0.0.0.0',
+        port=443,
+        ssl_context=(
+            '/etc/letsencrypt/live/ybenzou.work/fullchain.pem',  
+            '/etc/letsencrypt/live/ybenzou.work/privkey.pem'   
+        )
+    )
